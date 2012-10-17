2f1ee63b346e546bc1e64777282dada31a81aeab
Sofia Denner <sofi.denner@gmail.com>
2012-10-17 01:26:06 -0300

    typo

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Poll(models.Model):
-    question = models.CharField(max_lenght=200)
+    question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
