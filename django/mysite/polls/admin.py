bca42a436f7a4284f95976bee76269e9526a46e0
sofi <sofi@popa.(none)>
2012-10-23 11:49:59 -0300

    cambios en el admin polls

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -15,4 +15,11 @@ class PollAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
 
+    list_display = ('question', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
 admin.site.register(Poll, PollAdmin)
