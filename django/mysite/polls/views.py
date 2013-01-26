179361c8bf25b4465385c1c07b91e77f43d8df84
sofi <sofi@popa.(none)>
2013-01-26 02:16:56 -0300

    render_to_response y template detail()

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,17 +1,17 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from polls.models import Poll
-from django.http import HttpResponse
+from django.http import Http404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
