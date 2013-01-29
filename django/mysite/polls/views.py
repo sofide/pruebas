ddfb49641683a515e9ed08a7e565134dbef9b720
sofi <sofi@popa.(none)>
2013-01-29 03:15:34 -0300

    Formulario para votar encuesta

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 from django.http import Http404
+from django.template import RequestContext
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-     p = get_object_or_404(Poll, pk=poll_id)
-     return render_to_response('polls/detail.html', {'poll': p})
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p},
+                                context_instance=RequestContext(request))
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
