0c224e5c23993a610507f9af846b9eb23521cafd
sofi <sofi@popa.(none)>
2013-02-26 05:08:42 -0300

    agregada la vista visor

--- django/org_fotos/fotos/views.py
+++ django/org_fotos/fotos/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
 def home(request):
     return render_to_response('fotos/home.html')
 
+def visor(request):
+
+    if request.GET['ruta'] == '':
+        return render_to_response('fotos/home.html', {
+            'error_message': "No ingreso la ruta!!!",
+            })
+
+    else:
+        return render_to_response('fotos/visor.html')
