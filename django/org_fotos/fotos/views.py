96f1903ac27e13e5b27eb25e2546f351f598bf71
sofi <sofi@popa.(none)>
2013-02-28 03:37:56 -0300

    Probando vista y html del visor de fotos

--- django/org_fotos/fotos/views.py
+++ django/org_fotos/fotos/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+import glob
 
 def home(request):
     return render_to_response('fotos/home.html')
 
 def visor(request):
-
-    if request.GET['ruta'] == '':
+    ruta = request.GET['ruta']
+    if ruta == '':
         return render_to_response('fotos/home.html', {
             'error_message': "No ingreso la ruta!!!",
             })
 
     else:
-        return render_to_response('fotos/visor.html')
+        fotos = glob.glob(ruta+'*.jpg')
+
+        if fotos == []:
+            return render_to_response('fotos/home.html', {
+            'error_message': "No hay ninguna foto en la carpeta especificada",
+            })
+        rFotos = []
+        for f in fotos: rFotos.append("'" + f + "'")
+        context = {'fotos': rFotos}
+        return render(request, 'fotos/visor.html', context)
+
