7dcc039518b44155259cec65ac7ffa6d1683fd9c
sofi <sofi@popa.(none)>
2013-02-28 04:37:04 -0300

    intentando hacer andar el visor

--- django/org_fotos/fotos/views.py
+++ django/org_fotos/fotos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 import glob
+import os
+import shutil
 
 def home(request):
     return render_to_response('fotos/home.html')
@@ -20,8 +22,12 @@ def visor(request):
             return render_to_response('fotos/home.html', {
             'error_message': "No hay ninguna foto en la carpeta especificada",
             })
-        rFotos = []
-        for f in fotos: rFotos.append("'" + f + "'")
-        context = {'fotos': rFotos}
+
+        if not os.path.isdir('temp'):
+            os.mkdir('temp')
+
+        shutil.copy(fotos[0], 'temp')
+        nombreFoto = os.path.basename(fotos[0])
+        context = {'foto': nombreFoto}
         return render(request, 'fotos/visor.html', context)
 
