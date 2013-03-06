6abba7de442566a73ffe3bf927907e200e016184
sofi <sofi@popa.(none)>
2013-03-06 03:28:16 -0300

    empezando a reescribir la vista visor

--- django/org_fotos/fotos/views.py
+++ django/org_fotos/fotos/views.py
@@ -16,16 +16,22 @@ def manejador_rutas(request):
             'error_message': "No ingreso la ruta!!!",
             })
 
-    else:
-        fotos = glob.glob(ruta+'*.jpg')
+    fotos = glob.glob(ruta + '*.jpg')
 
     if fotos == []:
             return render_to_response('fotos/home.html', {
             'error_message': "No hay ninguna foto en la carpeta especificada",
             })
-
-    nombreFoto = os.path.basename(fotos[0])
-    return render_to_response('fotos/manejador_rutas.html', {'foto': nombreFoto})
-
+    request.session['carpeta_acomodar'] = ruta
+    return HttpResponseRedirect(reverse('fotos:visor'))
 
 def visor(request):
+    if 'carpeta_acomodar' not in request.session:
+        return render_to_response('fotos/home.html', {
+            'error_message': "Por favor, ingrese la ruta de la carpeta a acomodar",
+            })
+    fotos = glob.glob(request.session['carpeta_acomodar'] + '*.jpg')
+    nombreFoto = os.path.basename(fotos[0])
+    return render_to_response('fotos/visor.html', {
+        'foto': nombreFoto})
+
