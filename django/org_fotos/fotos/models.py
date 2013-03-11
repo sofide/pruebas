86f9b0a47626730919effe1f60dba10a8e1af951
Sofia Denner <sofi.denner@gmail.com>
2013-03-11 04:01:27 -0300

    creado el modelo Carpeta

--- django/org_fotos/fotos/models.py
+++ django/org_fotos/fotos/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Carpeta(models.Model):
+    ruta = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=7) #valores posibles 'origen' o 'destino'
